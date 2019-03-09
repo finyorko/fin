54d7f8fb350c1072e0165cba8a1a817483c67c04
finyorko <finyorko@163.com>
2019-03-09 21:26:53 +0800

    2019.03.09

---
+++ catkin_ws/build/Myrobot/CMakeFiles/FUN.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FUN.dir/src/talker.cpp.o"
+  "/home/finyorko/catkin_ws/devel/lib/Myrobot/FUN.pdb"
+  "/home/finyorko/catkin_ws/devel/lib/Myrobot/FUN"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FUN.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ catkin_ws/build/Myrobot/CMakeFiles/FUNNY.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FUNNY.dir/src/listener.cpp.o"
+  "/home/finyorko/catkin_ws/devel/lib/Myrobot/FUNNY.pdb"
+  "/home/finyorko/catkin_ws/devel/lib/Myrobot/FUNNY"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FUNNY.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
